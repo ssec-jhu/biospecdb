534012af0bab9006454f4046cc0ba9a465c91946
James Noss <jnoss@jhu.edu>
2023-05-12 13:21:01 -0400

    More stuff from tutorial...
    Signed-off-by: James Noss <jnoss@jhu.edu>

--- biospecdb/apps/uploader/urls.py
+++ biospecdb/apps/uploader/urls.py
@@ -7,6 +7,6 @@ app_name = "uploader"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:patient_id>/", views.patients, name="patients"),
+    path("<int:patient_id>/", views.patient_data, name="patient_data"),
     path("<int:patient_id>/samples", views.samples, name="samples"),
 ]
--- biospecdb/apps/uploader/views.py
+++ biospecdb/apps/uploader/views.py
@@ -1,17 +1,24 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from uploader.models import Patient
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the uploader index.")
+    all_patients = Patient.objects.order_by("-age")
+    context = {"patient_list": all_patients}
+    return render(request, "uploader/index.html", context)
 
 
-def patients(request, patient_id):
-    meta = Patient.objects.get(patient_id=patient_id)
-    return HttpResponse(f"Meta data for patient '{patient_id}': {meta}.")
+def patient_data(request, patient_id):
+    patient = get_object_or_404(Patient, pk=patient_id)
+    context = {"patient": patient,
+               "patient_fields": patient._meta.fields
+               }
+
+    return render(request, "uploader/patients.html", context)
 
 
 def samples(request, patient_id):
-    data = Patient.objects.get(patient_id=patient_id).data
+    data = get_object_or_404(Patient, pk=patient_id).data
     return HttpResponse(f"Bios sample data for patient '{patient_id}': {data}.")
